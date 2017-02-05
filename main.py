f8278599da8912b90cd2666fe1ef1c95fd03ac96
Balazs Pekar <balazs.pekar@gmail.com>
2017-02-05 21:39:42 +0100

    its something

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from sensor.sensor import Sensor
+from modules.sensor import Sensor
+from modules.player import Player
 
 
 def main():
     sensor = Sensor()
-    while True:
-        print("Distance received: " + str(sensor.get_distance()))
+    #while True:
+    #    print("Distance received: " + str(sensor.get_distance()))
+
+    player = Player()
+    player.load_file()    
+    player.play()
+    #print("visszatert")
 
 if __name__ == "__main__":
     main()
---
+++ modules/player.py
@@ -0,0 +1,63 @@
+import pygame
+import threading
+
+class Player:
+
+    def __init__(self):
+        self.file = '/zene.mp3'
+        pygame.init()
+        pygame.mixer.init()
+
+    def play(self):
+        threading.Thread(target=self._play).start()
+        print("Playing...")
+
+    
+    def _play(self):
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)            
+   
+
+    def pause(self):
+        pass
+
+
+    def next(self):
+        pass
+
+
+    def prev(self):
+        pass    
+
+
+    def volume_up(self):
+        pygame.mixer.music.set_volume(self.get_current_volume() + 0.5)        
+
+
+    def volume_down(self):
+        pygame.mixer.music.set_volume(self.get_current_volume() - 0.5)
+
+
+    def load_file(self):
+        pygame.mixer.music.load(self.file)
+
+
+    def reload_music_library(self):
+        pass
+
+    
+    def get_current_volume(self):
+        return pygame.mixer.music.get_volume()
+
+
+
+#c = 100
+#    if c >= 0:
+#        print("currvol " + str(currvol))
+#        pygame.mixer.music.set_volume(currvol - 0.005)   
+#    else:
+#        pygame.mixer.music.stop()
+#    pygame.time.Clock().tick(10)
+#    c -= 1
+
